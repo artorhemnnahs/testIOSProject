017cab3f0ea0aa38bf57f5d89b43a8e1f2b2e587
Shann M Mehrotra <smmehrot@us.ibm.com>
2014-12-14 21:58:57 -0500

    Initial Commit

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by Shann M Mehrotra on 12/14/14.
+//  Copyright (c) 2014 Shann M Mehrotra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
