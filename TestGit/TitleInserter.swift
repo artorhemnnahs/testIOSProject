d037eed35a128f1d546b2c3d0c157f21d85eb76a
Shann M Mehrotra <smmehrot@us.ibm.com>
2014-12-14 22:04:15 -0500

    Added files

---
+++ TestGit/TitleInserter.swift
@@ -0,0 +1,15 @@
+//
+//  TitleInserter.swift
+//  TestGit
+//
+//  Created by Shann M Mehrotra on 12/14/14.
+//  Copyright (c) 2014 Shann M Mehrotra. All rights reserved.
+//
+
+import UIKit
+
+class TitleInserter: NSObject {
+    class func setTitleToPooForViewController(aViewController:UIViewController) {
+        aViewController.title = "Poo"
+    }
+}
