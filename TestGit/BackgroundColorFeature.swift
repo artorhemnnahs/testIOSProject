32f1a5bd6bb7a020412ad782b2e40db6bc761683
Shann M Mehrotra <smmehrot@us.ibm.com>
2014-12-14 22:10:33 -0500

    New BG Colors

---
+++ TestGit/BackgroundColorFeature.swift
@@ -0,0 +1,16 @@
+//
+//  BackgroundColorFeature.swift
+//  TestGit
+//
+//  Created by Shann M Mehrotra on 12/14/14.
+//  Copyright (c) 2014 Shann M Mehrotra. All rights reserved.
+//
+
+import UIKit
+
+class BackgroundColorFeature: NSObject {
+    class func setBackgroundColorForVC(aViewController:UIViewController) {
+        aViewController.view.backgroundColor = UIColor.greenColor()
+    }
+
+}
