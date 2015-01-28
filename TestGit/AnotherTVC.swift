ff501c142cc261f61bc0a125879f116f94005efb
Shann M Mehrotra <smmehrot@Shanns-MacBook-Pro-2.local>
2015-01-27 23:09:50 -0800

    Another tvc

---
+++ TestGit/AnotherTVC.swift
@@ -0,0 +1,13 @@
+//
+//  AnotherTVC.swift
+//  TestGit
+//
+//  Created by Shann M Mehrotra on 1/27/15.
+//  Copyright (c) 2015 Shann M Mehrotra. All rights reserved.
+//
+
+import UIKit
+
+class AnotherTVC: UITableViewController {
+
+}
